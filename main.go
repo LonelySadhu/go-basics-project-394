adbe31eff0ae1549c8785fcea40d7833a8c8a688
Andrey Novikov <novikovandrey1984@gmail.com>
2026-07-13 13:45:38 +0300

    added checking password

--- main.go
+++ main.go
@@ -8,4 +8,9 @@ func main() {
 	fmt.Println(GeneratePassword(12, 123, true, true, true))
 	fmt.Println(GeneratePassword(8, 1, false, false, false))
 	fmt.Println(GeneratePassword(-3, 42, true, true, false))
+	fmt.Println(CheckPassword("abc"))
+	fmt.Println(CheckPassword("abcdefgh"))
+	fmt.Println(CheckPassword("abcdef1234"))
+	fmt.Println(CheckPassword("Abcdef1234"))
+	fmt.Println(CheckPassword("Abcdef123!"))
 }
--- password_generator.go
+++ password_generator.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
 const lowercase = "abcdefghijklmnopqrstuvwxyz"
 const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 const digits = "0123456789"
@@ -33,4 +38,54 @@ func GeneratePassword(length, seed int, useUppercase, useDigits, useSpecial bool
 		result = result + string(alphabet[index])
 	}
 	return result
+}
+
+func CheckPassword(password string) string {
+	estimate := 0
+	if len(password) == 0 {
+		return ""
+	}
+	for _, s := range lowercase {
+		if strings.ContainsRune(password, s) {
+			estimate = 2
+			break
+		}
+	}
+	if len(password) <= 3 {
+		estimate -= 1
+	}
+
+	for _, s := range digits {
+		if strings.ContainsRune(password, s) {
+			estimate += 1
+			break
+		}
+	}
+	for _, s := range uppercase {
+		if strings.ContainsRune(password, s) {
+			estimate += 1
+			break
+		}
+	}
+	for _, s := range special {
+		if strings.ContainsRune(password, s) {
+			estimate += 1
+			break
+		}
+	}
+	answer := fmt.Sprintf("пароль (оценка %d из 5)", estimate)
+	switch estimate {
+	case 1:
+		return "Слабый " + answer
+	case 2:
+		return "Слабый " + answer
+	case 3:
+		return "Средний " + answer
+	case 4:
+		return "Надежный " + answer
+	case 5:
+		return "Очень надежный " + answer
+	default:
+		return ""
+	}
 }
