4b1512531bd9854b14d44c58f396996cf61cc161
Andrey Novikov <novikovandrey1984@gmail.com>
2026-07-13 11:24:23 +0300

    added uppercase and digits

--- main.go
+++ main.go
@@ -3,6 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(GeneratePassword(5))
-	fmt.Println(GeneratePassword(30))
+	fmt.Println(GeneratePassword(30, true, true))
+	fmt.Println(GeneratePassword(30, false, false))
 }
--- password_generator.go
+++ password_generator.go
@@ -1,9 +1,16 @@
 package main
 
 const lowercase = "abcdefghijklmnopqrstuvwxyz"
+const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const digits = "0123456789"
 
-func GeneratePassword(length int) string {
+func GeneratePassword(length int, useUppercase, useDigits bool) string {
 	alphabet := lowercase
+	if useUppercase {
+		alphabet += uppercase
+	} else if useDigits {
+		alphabet += digits
+	}
 	len := len(alphabet)
 	result := ""
 	for i := 0; i < length; i++ {
